6d9404d1e5bc9f3f7b13d5ad9eb38b3eb9846302
suleymannmeral <115658595+suleymannmeral@users.noreply.github.com>
2022-10-28 00:30:02 +0300

    Diktörtgen Alanı Bulma

---
+++ diktortgenalanibulma/diktortgen-alani-bulma.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+int  uzunkenar , kisakenar , alan;
+printf("uzun kenar uzunlugunun giriniz :" );
+scanf("%d" , &uzunkenar);
+printf("kisa kenar uzunlugunu giriniz : ");
+scanf("%d" , &kisakenar);
+alan= uzunkenar*kisakenar;
+printf("diktortgenin alani %d dir" , alan);
+
+
+
+getch();
+
+}
+
+
+
+
+
+
+
+
+
